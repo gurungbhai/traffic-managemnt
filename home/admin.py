bad513d2c7ff3e95c04bca6d3572851ece20f86e
Rajan Gurung <59998674+gurungbhai@users.noreply.github.com>
2021-04-30 21:12:43 +0545

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from home.models import Contact
+from home.models import Profiles
+
+
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(Profiles)
+
+
+
